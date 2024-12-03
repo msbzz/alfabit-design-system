6db27892207696137adccffea648b888978dbba0
Marco <mbarozzi@gmail.com>
2024-12-03 08:00:13 -0300

    variações do botão

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -2,38 +2,41 @@ import React from "react";
 
 export type ButtonProps = {
   children: React.ReactNode;
-  /**
-   * Classe CSS adicional para estilização personalizada
-   */
   className?: string;
-  disabled?:boolean;
-  /**
-   * Callback acionado ao clicar no botão
-   */
+  variant?:'primary'|'secondary'|'tertiary';
   onClick?: () => void;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button: React.FC<ButtonProps> = ({ children, className,disabled, onClick, ...props }) => {
-  
-  const genetateStyles='rounded-md px-[32px] py-[12px] ';
+function getVariant(variant:ButtonProps['variant'],disabled:ButtonProps['disabled']){
+   
   const usingStyles ='text-white bg-primary hover:bg-primary/80 active:shadow-lg transition-all duration-200'
-  
-  const Btn = (classes:string)=>{
-     return (
-      <button
-      onClick={onClick}
-      disabled={disabled}
-      className={`order-spacing-0 ${className} ${classes} ${genetateStyles}`}      
-      {...props}
-    >
-      {children}
-    </button>
 
-     )
+  switch(variant){
+    case 'primary':
+      return disabled ?'bg-disabled text-disabled':`bg-primary text-white  ${usingStyles}` 
+    case 'secondary':
+      return disabled ?'bg-disabled text-disabled':`bg-secondary text-primary   ${usingStyles}` 
+    case 'tertiary':
+      return disabled ?'bg-disabled text-disabled':'' 
+    default:
+        return disabled ?'':''  
   }
+
+
+}
+
+const Button: React.FC<ButtonProps> = ({variant='primary' ,children, className,disabled, onClick, ...props }) => {
   
+   
   return (
-        Btn(disabled?'bg-disabled text-disabled': `${usingStyles}`)
+    <button
+    onClick={onClick}
+    disabled={disabled}
+    className={`rounded-md px-[32px] py-[12px]  order-spacing-0 ${getVariant(variant,disabled)} ${className} `}      
+    {...props}
+  >
+    {children}
+  </button>
       );
 };
 
